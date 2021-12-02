0defd0fe09b73bd20ddeaf8380473995a2350d39
Alexey Pogrebnikov <a.pogrebnikov@yandex.ru>
2021-12-02 13:03:01 +0500

    Extract a server project filename from args.

---
+++ ConsoleClient/ConsoleArgs.cs
@@ -0,0 +1,15 @@
+﻿namespace HttpMock.ConsoleClient
+{
+	public class ConsoleArgs
+	{
+		public string ServerProjectFileName { get; set; }
+
+		public static ConsoleArgs Parse(string[] args)
+		{
+			return new ConsoleArgs
+			{
+				ServerProjectFileName = args[0]
+			};
+		}
+	}
+}
